e778d548e941512dafbc8598a51cbdf72ee856b5
swiftyspiffy (cole) <swiftyspiffy@gmail.com>
2018-02-13 15:46:12 -0700

    Latest extension update .4

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var panel_token;
 var announce = true;
 var authenticated = false;
+var version = 4;
+var username;
+var base_api = "https://burkeblack.tv/extensions/burkes_booty/api.php";
 
 window.Twitch.ext.onAuthorized(function(auth) {
     panel_token = auth.token;
@@ -24,41 +27,29 @@ window.Twitch.ext.onError(function(error) {
 });
 
 $(document).ready(function() {
+    $('#giveaway_clear').click(function() {
+        clearGiveawaySubmissionInputs();
+    });
+    $('#giveaway_submit').click(function() {
+        submitGiveaway($('#giveaway_name').val(), $('#giveaway_donator').val(), $('#giveaway_type').val(), $('#giveaway_data').val(), $('#giveaway_doubloons').val(), $('#giveaway_non_sub').is(':checked'));
+    });
+	$('#wins_link').click(function() {
+		handleMenuLinks('wins');
+	});
     $('#credit_link').click(function() {
-        if($('#credit_info').is(":visible")) {
-            if(!($('#soundbytes').is(":visible")) && authed) {
-                $('#soundbytes').show();
-            }
-            $('#credit_info').hide();
-        } else {
-            $('#soundbytes').hide();
-            $('#credit_info').show();
-        }
+        handleMenuLinks('credit');
+    });
+    $('#giveaway_submission_link').click(function() {
+        handleMenuLinks('giveaway_submission');
     });
     $('#settings_link').click(function() {
-        if($('#settings_info').is(":visible")) {
-            $('#settings_info').hide();
-            if(!($('#soundbytes').is(":visible")) && authed) {
-                $('#soundbytes').show();
-            }
-        } else {
-            $('#settings_info').show();
-            $('#soundbytes').hide();
-        }
+        handleMenuLinks('settings');
     });
     $('#announce_yes_button').click(function() {
-        $('#announce_yes_button').addClass("settings_button_selected");
-        $('#announce_no_button').removeClass("settings_button_selected");
-        $('#announce_yes_button').prop('disabled', true);
-        $('#announce_no_button').prop('disabled', false);
-        announce = true;
+        handleAnnounce(true);
     });
     $('#announce_no_button').click(function() {
-        $('#announce_no_button').addClass("settings_button_selected");
-        $('#announce_yes_button').removeClass("settings_button_selected");
-        $('#announce_yes_button').prop('disabled', false);
-        $('#announce_no_button').prop('disabled', true);
-        announce = false;
+        handleAnnounce(false);
     });
 });
 
@@ -89,16 +80,22 @@ function registerEvents() {
         getSoundbytes($('#soundbyte_search').val(), $('#soundbyte_genre').val(), page);
     });
     $('#refresh_stats').click(function(){
-       getStats();
+		if($("#soundbytes").is(':visible')) {
+			getStats(true);
+			alertSuccess("Doubloon/soundbyte credits refreshed!");
+		} else if($('#wins_info').is(':visible')) {
+			getWinnings();
+			alertSuccess("Raffle and !Games refreshed!");
+		}
     });
 }
 
 function authing() {
-    getStats();
+    getStats(false);
     registerEvents();
 }
 
-function authed(username, doubloons, soundbyte_credits) {
+function authed(username, doubloons, soundbyte_credits, skipMsg) {
     authenticated = true;
     $('#stats').show();
     $('#welcome').hide();
@@ -108,26 +105,35 @@ function authed(username, doubloons, soundbyte_credits) {
     $('#doubloon_count').html(doubloons);
     $('#soundbyte_credit_count').html(soundbyte_credits);
     $('#soundbytes').show();
-    alertSuccess("Successfully connected to your account!");
+    $('#messages').show();
+    $('#giveaway_submission_img').show();
+    $('#giveaway_donator').val(username);
+	$('#wins_img').show();
+	$('#settings_img').show();
+    if(!skipMsg)
+        alertSuccess("Successfully connected to your account!");
     getGenres();
     getSoundbytes("", "all", 1);
+	getWinnings();
 }
 
-function getStats() {
+function getStats(skipMsg) {
     if(panel_token == null)
         return;
     $.ajax({
-        url: 'https://burkeblack.tv/extensions/burkes_booty/api.php?action=stats',
+        url: base_api + '?action=stats',
         type: 'get',
         headers: {
             "x-extension-jwt": panel_token,
-            accept: "application/json"
+            accept: "application/json",
+            version: version
         },
         dataType: 'json',
         success: function(data) {;
             if(data.successful) {
                 $('#pagination').show();
-                authed(data.username, data.doubloons, data.soundbyte_credits);
+                username = data.username;
+                authed(data.username, data.doubloons, data.soundbyte_credits, skipMsg);
             } else {
                 $('#pagination').hide();
                 alertFail(data.message);
@@ -140,11 +146,12 @@ function getGenres() {
     if(panel_token == null)
         return;
     $.ajax({
-        url: 'https://burkeblack.tv/extensions/burkes_booty/api.php?action=genres',
+        url: base_api + '?action=genres',
         type: 'get',
         headers: {
             "x-extension-jwt": panel_token,
-            accept: "application/json"
+            accept: "application/json",
+            version: version
         },
         dataType: 'json',
         success: function(data) {
@@ -154,7 +161,7 @@ function getGenres() {
                 $('#soundbyte_genre').empty();
                 $('#soundbyte_genre').append('<option selected="selected" value="all">All</option>');
                 $.each(genres, function(ind, genre) {
-                   $('#soundbyte_genre').append('<option value="' + genre.text + '">' + genre.text +'</option>');
+                    $('#soundbyte_genre').append('<option value="' + genre.text + '">' + genre.text +'</option>');
                 });
             } else {
                 $('#pagination').hide();
@@ -166,11 +173,12 @@ function getGenres() {
 
 function getSoundbytes(searchText, searchGenre, page) {
     $.ajax({
-        url: 'https://burkeblack.tv/extensions/burkes_booty/api.php?action=soundbytes&search_text=' + searchText + '&search_genre=' + searchGenre + '&page=' + page,
+        url: base_api + '?action=soundbytes&search_text=' + searchText + '&search_genre=' + searchGenre + '&page=' + page,
         type: 'get',
         headers: {
             "x-extension-jwt": panel_token,
-            accept: "application/json"
+            accept: "application/json",
+            version: version
         },
         dataType: 'json',
         success: function(data) {
@@ -193,7 +201,7 @@ function getSoundbytes(searchText, searchGenre, page) {
                         '                </tr>';
                     $('#soundbyte_Table_body').append(soundbyteData);
                     $('#sb_' + soundbyte.id).on('click', function() {
-                         sendSoundbyte(soundbyte.id);
+                        sendSoundbyte(soundbyte.id);
                     });
                 });
             } else {
@@ -205,29 +213,63 @@ function getSoundbytes(searchText, searchGenre, page) {
 }
 
 function sendSoundbyte(soundbyteId) {
-    var url;
-    if(announce)
-        url = 'https://burkeblack.tv/extensions/burkes_booty/api.php?action=sendSoundbyte&soundbyteId=' + soundbyteId + '&announce=1';
-    else
-        url = 'https://burkeblack.tv/extensions/burkes_booty/api.php?action=sendSoundbyte&soundbyteId=' + soundbyteId + '&announce=0';
+    var url = base_api + '?action=sendSoundbyte&soundbyteId=' + soundbyteId + '&announce=';
+	var url = announce ? url + "1" : url + "0";
 
     $.ajax({
         url: url,
         type: 'get',
         headers: {
             "x-extension-jwt": panel_token,
-            accept: "application/json"
+            accept: "application/json",
+            version: version
         },
         dataType: 'json',
         success: function(data) {
             if(data.successful) {
                 alertSuccess("Soundbyte[" + soundbyteId + "] successfully sent!");
-                getStats();
+                getStats(true);
             } else {
                 alertFail(data.message);
             }
         }
     });
+}
+
+function getWinnings() {
+	if(panel_token == null)
+        return;
+    $.ajax({
+        url: base_api + '?action=winnings',
+        type: 'get',
+        headers: {
+            "x-extension-jwt": panel_token,
+            accept: "application/json",
+            version: version
+        },
+        dataType: 'json',
+        success: function(data) {
+            if(data.successful) {
+				$('#raffle_wins').empty();
+				$('#exgame_redemptions').empty();
+                var raffleWins = data['raffle_wins'];
+				var exgameRedemptions = data['exgame_redemptions'];
+				$.each(raffleWins, function(id, win) {
+					insertWinIntoTable('raffle_wins', win['game'], win['key'], win['date']);
+				});
+				$.each(exgameRedemptions, function(id, win) {
+					insertWinIntoTable('exgame_redemptions', win['game'], win['key'], win['date']);
+				});
+            } else {
+                alertFail(data.message);
+            }
+        }
+    });
+}
+
+function insertWinIntoTable(table, game, key, date) {
+	var row = "<tr><td><b>Name: </b>" + game + "<br><b>Key: </b>" + key + "<br><b>Date: </b>" + date + "</td></tr>";
+	$('#' + table).append(row);
 }
 
 function alertSuccess(msg) {
@@ -240,4 +282,65 @@ function alertFail(msg) {
     $('#fail_msg').html(msg);
     $('#success').hide();
     $('#fail').show();
+}
+
+function clearGiveawaySubmissionInputs() {
+    $("#giveaway_name").val("");
+    $("#giveaway_donator").val(username);
+    $("#giveaway_data").val("");
+    $("#giveaway_doubloons").val("0");
+}
+
+function submitGiveaway(name, donator, type, data, doubloons, non_sub_only) {
+    name = btoa(name);
+    donator = btoa(donator);
+    data = btoa(data);
+    $.ajax({
+        url: base_api + "?action=giveawaySubmission&name=" + name + "&donator=" + donator + "&type=" + type + "&data=" + data + "&doubloons=" + doubloons + "&non_sub_only=" + non_sub_only,
+        type: 'get',
+        headers: {
+            "x-extension-jwt": panel_token,
+            accept: "application/json",
+            version: version
+        },
+        dataType: 'json',
+        success: function(data) {
+            if(data.successful) {
+                alertSuccess("Giveaway successfully submitted!");
+                clearGiveawaySubmissionInputs();
+            } else {
+                alertFail(data.message);
+            }
+        }
+    });
+}
+
+function handleAnnounce(res) {
+	if(res) {
+		$('#announce_yes_button').addClass("settings_button_selected");
+		$('#announce_no_button').removeClass("settings_button_selected");
+		$('#announce_yes_button').prop('disabled', true);
+		$('#announce_no_button').prop('disabled', false);
+	} else {
+		$('#announce_no_button').addClass("settings_button_selected");
+        $('#announce_yes_button').removeClass("settings_button_selected");
+        $('#announce_yes_button').prop('disabled', false);
+        $('#announce_no_button').prop('disabled', true);
+	}
+	announce = res;
+}
+
+function handleMenuLinks(clicked_on) {
+	var clicked_data = '#' + clicked_on + '_info';
+	if($(clicked_data).is(':visible')) {
+		$(clicked_data).hide();
+		$('#soundbytes').show();
+	} else {
+		$('#wins_info').hide();
+		$('#credit_info').hide();
+		$('#giveaway_submission_info').hide();
+		$('#settings_info').hide();
+		$('#soundbytes').hide();
+		$(clicked_data).show();
+	}
 }
